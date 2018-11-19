73bacf0184807c903f4120765e4f7b2f38e25ee6
Nick Bikker <nickbikkerprive@gmail.com>
2018-11-19 14:59:22 +0100

    begin arrays
    start with arrays made

---
+++ 1/main.js
@@ -0,0 +1,6 @@
+var DaysofWeek = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag', 'zaterdag', 'zondag'];
+
+
+console.log(DaysofWeek);
+document.getElementById('daysweek').innerHTML = DaysofWeek;
+document.getElementById('workdays').innerHTML = DaysofWeek[0][1];
